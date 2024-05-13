e3e6bc6515c8909937f707c254c5fb421f4491f4
veresmar <veres.maria.dev@gmail.com>
2024-05-13 14:58:33 +0700

    return to the old code
    - prev ver of js, html, css
    - add arrows
    - add shadows

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const galleryContainer = document.querySelector(".gallery-container");
-  const gallery = galleryContainer.querySelector(".gallery");
-  const prevBtn = galleryContainer.querySelector(".prev");
-  const nextBtn = galleryContainer.querySelector(".next");
-  const dotsContainer = galleryContainer.querySelector(".dots-container");
+    const gallery = document.querySelector(".gallery");
+    const images = document.querySelectorAll(".gallery img");
+    const prevBtn = document.querySelector(".prev");
+    const nextBtn = document.querySelector(".next");
+    const dotsContainer = document.querySelector(".dots-container");
   
-  // Получение массива изображений из глобальной области видимости
-  const images = window.images || []; 
+    let index = 0;
+    
+    
+    function loadImage(n) {
+        const image = new Image();
+        image.onload = function() {
+            console.log("Image loaded:", image.src);
+            gallery.appendChild(image);
+        };
+        image.src = images[n].src;
+    }
 
-  let index = 0;
+    loadImage(index);
 
-  function loadImage(n) {
-    const image = new Image()
-    image.src = images[n]
-  }
 
-  function showImage(n) {
-      index = (n + images.length) % images.length;
-      gallery.style.backgroundImage = `url('${images[index]}')`;
-      updateDots();
-      loadImage(n + 1)
-  }
-
-  function updateDots() {
-      const dots = dotsContainer.querySelectorAll(".dot");
-      dots.forEach((dot, i) => {
-          dot.classList.remove("active");
-          if (i === index) {
-              dot.classList.add("active");
-          }
-      });
-  }
-
-  function nextImage() {
-      showImage(index + 1);
-  }
-
-  function prevImage() {
-      showImage(index - 1);
-  }
-
-  // Создание кружков
-  images.forEach((_, i) => {
-      const dot = document.createElement("span");
-      dot.classList.add("dot");
-      dot.addEventListener("click", () => showImage(i));
-      dotsContainer.appendChild(dot);
+    function showImage(n) {
+        index = (n + images.length) % images.length;
+        gallery.style.transform = `translateX(-${index * 100}%)`;
+        updateDots();
+    }
+  
+    function updateDots() {
+        const dots = document.querySelectorAll(".dot");
+        dots.forEach((dot, i) => {
+            dot.classList.remove("active");
+            if (i === index) {
+                dot.classList.add("active");
+            }
+        });
+    }
+  
+    function nextImage() {
+        showImage(index + 1);
+    }
+  
+    function prevImage() {
+        showImage(index - 1);
+    }
+  
+    function setDotClickEvent(dot, i) {
+        dot.addEventListener("click", () => {
+            showImage(i);
+        });
+    }
+  
+    // Создание кружков
+    images.forEach((_, i) => {
+        const dot = document.createElement("span");
+        dot.classList.add("dot");
+        dot.addEventListener("click", () => showImage(i));
+        dotsContainer.appendChild(dot);
+    });
+  
+    // Переключение фото по клику на стрелки
+    prevBtn.addEventListener("click", () => {
+        prevImage();
+    });
+    nextBtn.addEventListener("click", () => {
+        nextImage();
+    });
+  
+    // Начальная активация первого кружка
+    updateDots();
+  
   });
-
-  // Переключение по клику на стрелки
-  prevBtn.addEventListener("click", prevImage);
-  nextBtn.addEventListener("click", nextImage);
-
-  // Начальная активация первого кружка
-  updateDots();
-  // Отображение первого изображения при загрузке страницы
-  showImage(0);
-});
